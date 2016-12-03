ddb745304664350df2573fd54681f18999b178e2
yehua gao <yehua gao>
2016-12-04 00:54:01 +0800

    main_layout.xml

--- app/src/main/java/com/yeyu/james/huiheart/UI/Activity/Main/MainActivity.java
+++ app/src/main/java/com/yeyu/james/huiheart/UI/Activity/Main/MainActivity.java
@@ -1,8 +1,33 @@
 package com.yeyu.james.huiheart.UI.Activity.Main;
 
+import android.view.View;
+
+import com.yeyu.james.huiheart.R;
+import com.yeyu.james.huiheart.UI.Activity.BaseActivity;
+
 /**
  * Created by James on 2016/12/3.
  */
 
-public class MainActivity {
+public class MainActivity extends
+        BaseActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    protected int getResultId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
 }
